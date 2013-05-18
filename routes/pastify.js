33d0649990f342028d80acf866d694b5d0bcfbe7
Tsung Wu <tsung.bz@gmail.com>
2013-05-19 00:10:32 +0800

    init proj

---
+++ routes/pastify.js
@@ -0,0 +1,35 @@
+var clip = require('clip');
+
+/*
+ * GET current clipboard
+ */
+var pastifing = '';
+
+exports.get = function(req, res){
+  res.send(JSON.stringify({
+	  text: clip.get()//pastifing
+  }));
+};
+
+exports.set = function(req, res) {
+	var clipboard = req.body.text;
+	console.log('setting paste: ' + clipboard);
+	if(typeof req.body.os != 'undefined' && req.body.os == 'ios') {
+		//set system clipboard
+		var command = "echo '" + clipboard + "' | pbcopy";
+		var exec = require('child_process').exec;
+		var child = exec(command,
+			function (error, stdout, stderr) {
+				//console.log('stdout: ' + stdout);
+				//console.log('stderr: ' + stderr);
+				if (error !== null) {
+					console.log('exec error: ' + error);
+				}
+			}
+		);
+	} else {
+		pastifing = clipboard;
+	}
+	console.log(pastifing);
+	res.send("success");
+};
